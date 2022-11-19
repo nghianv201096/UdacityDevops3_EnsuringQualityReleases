d88be8a6ca15baeecee73d95bc5271718086101a
nghianv <nghia.nv.hust@gmail.com>
2022-11-19 16:17:43 +0700

    update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -10,6 +10,8 @@ def timestamp():
 
 # Start the browser and login with standard_user
 def login(user, password):
+    
+    # go to login page.
     print(timestamp() + 'Starting the browser...')
     options = ChromeOptions()
     options.add_argument('--no-sandbox')
@@ -18,24 +20,27 @@ def login(user, password):
     driver = webdriver.Chrome(options=options, executable_path="automatedtesting/selenium/chromedriver")
     print(timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-    # login
+    
+    # login to the website.
     driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
     driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
     driver.find_element(By.ID, "login-button").click()
     product_label = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_item_name']").text
     assert "Sauce Labs Backpack" in product_label
-    print(timestamp() + 'Login with username {:s} and password {:s} successfully.'.format(user, password))
+    print(timestamp() + 'Login successfully with username {:s} and password {:s}.'.format(user, password))
+    
     return driver
 
 def add_cart(driver, n_items):
     for i in range(n_items):
-        element = "a[id='item_" + str(i) + "_title_link']"  # Get the URL of the product
-        driver.find_element(By.CSS_SELECTOR, element).click()  # Click the URL
-        driver.find_element(By.CSS_SELECTOR,"button.btn_primary.btn_inventory").click()  # Add the product to the cart
-        product = driver.find_element(By.CSS_SELECTOR,"div[class='inventory_details_name large_size']").text  # Get the name of the product from the page
-        print(timestamp() + product + " added to shopping cart.")  # Display message saying which product was added
-        driver.find_element(By.CSS_SELECTOR,"button.inventory_details_back_button").click()  # Click the Back button
-    print(timestamp() + '{:d} items are all added to shopping cart successfully.'.format(n_items))
+        element = "a[id='item_" + str(i) + "_title_link']"  
+        driver.find_element(By.CSS_SELECTOR, element).click()  
+        driver.find_element(By.CSS_SELECTOR,"button.btn_primary.btn_inventory").click()  
+        product = driver.find_element(By.CSS_SELECTOR,"div[class='inventory_details_name large_size']").text  
+        print(timestamp() + product + " is added to the shopping cart.")  
+        driver.find_element(By.CSS_SELECTOR,"button.inventory_details_back_button").click()
+
+    print(timestamp() + '{:d} items are all added to the shopping cart successfully.'.format(n_items))
 
 def remove_cart(driver, n_items):
     for i in range(n_items):
@@ -43,9 +48,9 @@ def remove_cart(driver, n_items):
         driver.find_element(By.CSS_SELECTOR,element).click()
         driver.find_element(By.CSS_SELECTOR,"button.btn_secondary.btn_inventory").click()
         product = driver.find_element(By.CSS_SELECTOR,"div[class='inventory_details_name large_size']").text
-        print(timestamp() + product + " removed from shopping cart.")  # Display message saying which product was added
+        print(timestamp() + product + " is removed from the shopping cart.")
         driver.find_element(By.CSS_SELECTOR,"button.inventory_details_back_button").click()
-    print(timestamp() + '{:d} items are all removed from shopping cart successfully.'.format(n_items))
+    print(timestamp() + '{:d} items are removed from the shopping cart successfully.'.format(n_items))
 
 
 if __name__ == "__main__":
@@ -54,8 +59,12 @@ if __name__ == "__main__":
     TEST_PASSWORD = 'secret_sauce'
     driver = login(TEST_USERNAME, TEST_PASSWORD)
     add_cart(driver, N_ITEMS)
+    print(timestamp() + 'Test adding items done!')
+    
     remove_cart(driver, N_ITEMS)
-    print(timestamp() + 'Selenium tests are all successfully completed!')
+    print(timestamp() + 'Test removing items done!')
+    
     driver.stop_client()
     driver.close()
     driver.quit()
+    print('Clean the client done!');
